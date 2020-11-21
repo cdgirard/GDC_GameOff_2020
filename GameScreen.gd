a07b76195363d22fa9a4b3fff203f5b9fe28b2d5
Charles Girard <cdgira@ship.edu>
2020-11-21 12:01:55 -0500

    Resource points added, status bars added.

--- GameScreen.gd
+++ GameScreen.gd
@@ -3,25 +3,66 @@ extends Node2D
 onready var Background = preload("res://game_background/GameBackground.tscn")
 onready var Player = preload("res://player/Player.tscn")
 onready var LargeAsteroid = preload("res://LargeAsteroid/LargeAsteroid.tscn")
+onready var GameScreenUI = preload("res://GameScreenUI.tscn")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var universeSprite = Sprite.new()
+var asteroids = {}
+var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	var bkg = Background.instance()
 	add_child(bkg)
-	var largeAsteroid = LargeAsteroid.instance()
-	Globals.active_asteroid = largeAsteroid
-	add_child(largeAsteroid)
-	var player = Player.instance()
+	universeSprite.texture = load("res://universe/universe_map.png")
+	create_map()
+	player = Player.instance()
 	player.position.y = -400
 	add_child(player)
+	add_child(GameScreenUI.instance())
+	determine_gravity_source()
 
+func determine_gravity_source() :
+	var min_dist = -1
+	for pos in asteroids :
+		var dist = (pos - player.position).length()
+		if min_dist == -1 :
+			min_dist = dist
+			Globals.active_asteroid = asteroids[pos]
+		elif min_dist > dist :
+			min_dist = dist
+			Globals.active_asteroid = asteroids[pos]
 
-
+func create_map() :
+	var info = universeSprite.texture.get_data()
+	info.lock()
+	var row = 0
+	
+	while row < info.get_height() :
+		var col = 0
+		while col < info.get_width() :
+			var blockLoc = Vector2(800*col,800*row)
+			var colorInfo = info.get_pixel(col,row)
+			var asteroid = null
+			if colorInfo.r == 0 and colorInfo.g == 0 and colorInfo.b == 0 : #black
+				asteroid = LargeAsteroid.instance()
+			if colorInfo.r == 1 and colorInfo.g == 0 and colorInfo.b == 0 : #red
+				asteroid = LargeAsteroid.instance()
+			if colorInfo.r == 0 and colorInfo.g == 1 and colorInfo.b == 0 : #green
+				asteroid = LargeAsteroid.instance()
+			if colorInfo.r == 0 and colorInfo.g == 0 and colorInfo.b == 1 : #blue
+				asteroid = LargeAsteroid.instance()
+			if colorInfo.r == 1 and colorInfo.g == 0 and colorInfo.b == 1 : #purple
+				asteroid = LargeAsteroid.instance()
+			if colorInfo.r == 0 and colorInfo.g == 1 and colorInfo.b == 1 : #Blue Green
+				asteroid = LargeAsteroid.instance()
+			if asteroid != null :
+					asteroid.position = blockLoc
+					asteroids[asteroid.position] = asteroid
+					add_child(asteroid)
+			col += 1
+		row += 1
+	info.unlock()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
--- LargeAsteroid/LargeAsteroid.gd
+++ LargeAsteroid/LargeAsteroid.gd
@@ -1,6 +1,6 @@
 extends StaticBody2D
 
-
+onready var BlueGems = preload("res://blue_gems/BlueGems.tscn")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,6 +8,7 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	attach_resources()
 	pass # Replace with function body.
 
 
@@ -22,3 +23,12 @@ func _process(delta):
 		rotation_degrees -= .5
 		Globals.asteroid_motion = true
 		Globals.asteroid_direction = -1
+
+func attach_resources() :
+	
+	var res_points = get_node("ResourcePoints")
+	for node in res_points.get_children() :
+		var res_node = BlueGems.instance()
+		res_node.scale.x = 1 / scale.x
+		res_node.scale.y = 1 / scale.y
+		node.add_child(res_node)
