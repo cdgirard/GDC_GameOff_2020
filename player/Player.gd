4ccbd8aa906b9175d737935b381f6fa4656d1fce
Charles Girard <cdgira@ship.edu>
2020-11-21 08:16:59 -0500

    Menu Screen linked to Game Screen, move sound effect added.

--- player/Player.gd
+++ player/Player.gd
@@ -10,6 +10,9 @@ export var speed = 100
 var prev_position
 var roll_mode = false
 var roll_timer = 0
+var sound_effect_mode = 0
+var sound_effects = false
+var dist_moved = 0
 
 func move():
 	pass
@@ -27,14 +30,16 @@ func move():
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	velocity = Vector2(0,0)
-	get_node("MoveOnAsteroidSound").play()
 	prev_position = position
 	#print(velocity)
 	pass
 		
 func _process(delta):
+	if sound_effects :
+		play_sound()
+	#if not get_node
 	var pos_change = (prev_position - position)
-	var dist_moved = pos_change.length()
+	dist_moved = pos_change.length()
 
 	prev_position = position
 	if dist_moved < 0.05 and not roll_mode:
@@ -54,7 +59,15 @@ func _process(delta):
 			angular_velocity = -5
 		elif Globals.asteroid_direction == -1 :
 			angular_velocity = 5
-
+			
+	if dist_moved < 0.2:
+		get_node("MoveOnAsteroidSoundV1").volume_db = -80
+		get_node("MoveOnAsteroidSoundV3").volume_db = -80
+	elif dist_moved < 0.4 :
+		get_node("MoveOnAsteroidSoundV1").volume_db = -15
+	else :
+		get_node("MoveOnAsteroidSoundV1").volume_db = -15
+		get_node("MoveOnAsteroidSoundV3").volume_db = -15
 	
 	if Globals.asteroid_motion :
 		roll_mode = false
@@ -77,3 +90,31 @@ func _physics_process(delta):
 	self.linear_velocity += gravity
 	#velocity += gravity
 	move()
+
+func play_sound() :
+	var playing = true
+	if (not get_node("MoveOnAsteroidSoundV1").playing) and (not get_node("MoveOnAsteroidSoundV2").playing) and (not get_node("MoveOnAsteroidSoundV3").playing) :
+		playing = false
+	if (not playing) and (sound_effect_mode == 0):
+		get_node("MoveOnAsteroidSoundV1").play()
+		sound_effect_mode = 1
+	elif (not playing) and (sound_effect_mode == 1):
+		get_node("MoveOnAsteroidSoundV3").play()
+		sound_effect_mode = 2
+	elif (not playing) and (sound_effect_mode == 2) :
+		get_node("MoveOnAsteroidSoundV2").play()
+		sound_effect_mode = 3
+	elif (not playing) and (sound_effect_mode == 3) :
+		get_node("MoveOnAsteroidSoundV3").play()
+		sound_effect_mode = 0
+
+func _on_Player_body_entered(body):
+	sound_effects = true
+	
+
+
+func _on_Player_body_exited(body):
+	sound_effects = false
+	if dist_moved > 0.4 :
+		if (sound_effect_mode == 2) or (sound_effect_mode == 0) :
+			sound_effect_mode += 1
